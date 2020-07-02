a201d572c9b88b9397e9ae11bac481b1dda36da8
Darren <62718844+bleib-neugierig881@users.noreply.github.com>
2020-07-01 20:38:24 -0700

    Update Project09_mod.ino

--- Project09_mod.ino
+++ Project09_mod.ino
@@ -5,23 +5,31 @@ int switchVal;
 const int switchValLow = 520;
 const int switchValHigh = 1023;
 void setup() {
-  // put your setup code here, to run once:
   pinMode(motorPin,OUTPUT);
   pinMode(switchPin,INPUT);
-  Serial.begin(9600);
+  Serial.begin(9600); // This serial comms just helps you see what ranges are being read 
+                      // from your variable resistance and the respective DC motor speed 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  switchVal = analogRead(switchPin);
-  Serial.print("Current power rating: ");
+  switchVal = analogRead(switchPin); // This is where your potentiometer needs to be connected in series to
+  Serial.print("Current power rating: "); // Power may not be most accurate term, but you get the idea
   Serial.print(switchVal);
+  
+  // The following two if statements guarantee the switch value never falls below or above a certain range, 
+  // which just smooths out the map function below. You may need to adjust these based on your resistor values.
   if (switchVal<switchValLow){
-    switchVal = switchValLow;
+    switchVal = switchValLow; 
   }
   if (switchVal>switchValHigh){
     switchVal = switchValHigh;
   }
-  int wheelSpeed = map(switchVal,switchValLow,switchValHigh,0,100);
-  analogWrite(9,wheelSpeed);
+  
+  int wheelSpeed = map(switchVal,switchValLow,switchValHigh,0,100); // Map, as mentioned in the video, sets your 
+                                                                    // input analog reading into a proportional response
+                                                                    // between a desired low and high point, see '0' and '100'
+                                                                    
+  analogWrite(9,wheelSpeed); // The mapped analogRead from swtichPin is then sent out through the Arduino's pin 9 with PWM control
+                             // The pulse width is determined by the wheelSpeed variable.
 }
